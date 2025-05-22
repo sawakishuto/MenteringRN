fa8d35e2f3f1edba3c4fcfed7976b349d36bd457
sawakishuto <swkshuto0208@icloud.com>
2025-05-22 17:32:52 +0900

    feat: もうちょいいい感じにした

--- app/index.tsx
+++ app/index.tsx
@@ -5,10 +5,10 @@ export default function HomeScreen() {
   const router = useRouter();
 
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-600 mb-4">Home Screen</Text>
+    <View className="flex-1 items-center justify-center bg-white border-6 border-red-500 gap-4">
+        <Text className="text-yellow-500 text-3xl font-bold border-2 border-blue-500" >TODOアプリ</Text>
       <TouchableOpacity
-        className="bg-blue-500 px-4 py-2 rounded-lg"
+        className="bg-blue-500 px-4 py-2 rounded-lg border-2 border-blue-500"
         onPress={() => router.push("/detail")}
       >
         <Text className="text-white font-semibold">Go to Detail</Text>
@@ -16,3 +16,11 @@ export default function HomeScreen() {
     </View>
   );
 }
+
+
+// これ見ながらデザイン作っていこう
+// https://nerdcave.com/tailwind-cheat-sheet
+
+
+// useState 
+// useEffect
